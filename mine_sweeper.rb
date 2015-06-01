4cba36f4ae96e1ee5d57c094c641486d6630705b
Scott+Sam <Scott+Sam@appacademy.io>
2015-06-01 12:12:27 -0400

    Refactor game class into board and game class.

--- mine_sweeper.rb
+++ mine_sweeper.rb
@@ -1,8 +1,6 @@
 
 class Game
 
-
-
   def self.new_game
     Game.new
   end
@@ -14,22 +12,49 @@ class Game
 
 
 
-  BOARD_SIZE = 9
-
   def initialize()
-    @board = init_board
+    @board = Board.new
+
+
     # play
   end
 
-
-
-  def init_board
-    Array.new(BOARD_SIZE) { Array.new(BOARD_SIZE, Cell.new) }
-  end
-
   def play
+    until won? || lost?
+      @board.display
+      player_move = get_move ##ex. [0, 0]
+      @board.check_move(player_move)
+
   end
 
+  def lost?
+    @board.losing_board?
+  end
+
+  def won?
+    @board.winning_board?
+  end
+
+  def get_move
+    loop do
+      move = []
+      print "Row:  "
+      move << gets.chomp.to_i
+      print "Col:  "
+      move << gets.chomp.to_i
+      break if move.all? { |m| (0..8).to_a.include?(m) }
+      puts "please use numbers between 0 and 8"
+    end
+
+    move
+  end
+
+
+
+end
+
+class Board
+  BOARD_SIZE = 9
   NEIGHBORS = [
     [1, 0]
     [1, 1]
@@ -41,14 +66,20 @@ class Game
     [-1, 1]
   ]
 
+  def initialize
+    @grid = Array.new(BOARD_SIZE) { Array.new(BOARD_SIZE, Cell.new) }
+    init_neighbors
+    init_values
+  end
+
   def init_neighbors
-    @board.each_with_index do |row, idx1|
+    @grid.each_with_index do |row, idx1|
       row.each_with_index do |cell, idx2|
         NEIGHBORS.each do |neighbor|
           rel_row = idx1 + neighbor[0]
           rel_col = idx2 + neighbor[1]
           unless (0..8).to_a.include?(rel_row) && (0..8).to_a.include?(rel_col)
-            cell.neighbors << @board[rel_row][rel_col]
+            cell.neighbors << @grid[rel_row][rel_col]
           end
         end
       end
@@ -58,7 +89,7 @@ class Game
   end
 
   def init_values
-    @board.each do |row|
+    @grid.each do |row|
       row.each do |cell|
         cell.neighbors.each do |neighbor|
           cell.value += 1 if neighbor.bomb == true
@@ -68,18 +99,36 @@ class Game
 
     nil
   end
+
+  def update_board
+
+
+  def winning_board?
+    @grid.all? do |row|
+      row.all? { |cell| cell.revealed || cell.flagged }
+    end
+  end
+
+  def losing_board?
+    @grid.any? do |row|
+      row.any? { |cell| cell.revealed && cell.bomb }
+    end
+  end
+
+
 end
 
 class Cell
-  attr_accessor :value, :bomb, :neighbors, :flag
+  attr_accessor :value, :bomb, :neighbors, :flagged
 
   def initialize
     @value = 0
     @bomb = set_bomb?
-    @flag = false
+    @flagged = false
     @revealed = false
     @neighbors = []
   end
+
 
   def set_bomb?
     rand(7) == 0
